19a0b261345202e2089bf77e97c879a8a2b8b677
Annie <annie.yiyunc@gmail.com>
2019-09-08 14:02:40 -0700

    pseudocode in game.js file

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+// make a wordList for user to guess
+
+
+// select a word in current word from wordList
+    // count how many letters are in the currentWord
+    // display the amount of letters in same number of _
+
+
+
+// When userInput a letter but is not in the current word...
+    // display the word in guessed-word
+    // remaining-guesses -1
+
+
+// When userInput a letter and it is in the current word...
+    // swap out the corrisponding "_" with the letter userInput
+
+
+// Repeat for upto 15 incorrect guesses
+    // remaining-guesses starts at 15
+
+
+
+// If remaining-guesses is >0 and currentWord completed, wins-count +1
+
+
+
+// If remaining guess =0 and word not guessed, wins-count doesn't change
